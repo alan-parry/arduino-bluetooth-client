c3ec924169d9e10cc688325446b9f3ccfc8d7c4c
Alan Parry <alan@parry.me.uk>
2016-02-05 11:06:23 +0000

    Initial import

---
+++ app/src/main/java/com/alanparry/arduinobluetoothclient/ConnectThread.java
@@ -0,0 +1,79 @@
+package com.alanparry.arduinobluetoothclient;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Message;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import android.util.Log;
+
+/**
+ * Created by alapar on 12/18/2015.
+ */
+public class ConnectThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothSocket mSocket;
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    public ConnectThread(BluetoothDevice device, BluetoothAdapter adapter) {
+        BluetoothSocket tmp = null;
+        this.mBluetoothAdapter = adapter;
+        this.mmDevice = device;
+        try {
+            tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {}
+        mmSocket = tmp;
+    }
+    public void run() {
+        byte[] buffer = new byte[1024];
+        int begin = 0;
+        int bytes = 0;
+        while (true) {
+            try {
+                bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
+                for(int i = begin; i < bytes; i++) {
+                    if(buffer[i] == "#".getBytes()[0]) {
+                        mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
+                        begin = i + 1;
+                        if(i == bytes - 1) {
+                            bytes = 0;
+                            begin = 0;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                break;
+            }
+        }
+    }
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
+    }
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            byte[] writeBuf = (byte[]) msg.obj;
+            int begin = (int)msg.arg1;
+            int end = (int)msg.arg2;
+
+            switch(msg.what) {
+                case 1:
+                    String writeMessage = new String(writeBuf);
+                    writeMessage = writeMessage.substring(begin, end);
+                    Log.d("ConnectThread", "message");
+                    break;
+            }
+        }
+    };
+}
+
